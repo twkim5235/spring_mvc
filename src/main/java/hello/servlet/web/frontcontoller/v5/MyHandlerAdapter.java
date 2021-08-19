91808d52381c2325ed4c7d0b19b29e6bb7b87308
twkim5235 <sssunggure@naver.com>
2021-08-19 14:30:18 +0900

    [ADD] ControllerV5 버전
    유연하게 Controller를 사용가능하게 회원관리 애플리케이션 구현
    - Controller를 유연하게 사용하기 위해 중간에 어댑터를 추가해줘서, V3, V4중 사용할 controller를 adapter에서 반환하여준다.

---
+++ src/main/java/hello/servlet/web/frontcontoller/v5/MyHandlerAdapter.java
@@ -0,0 +1,15 @@
+package hello.servlet.web.frontcontoller.v5;
+
+import hello.servlet.web.frontcontoller.ModelView;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public interface MyHandlerAdapter {
+
+    boolean supports(Object handler);
+
+    ModelView handle(HttpServletRequest request, HttpServletResponse response, Object handler) throws ServletException, IOException;
+}
